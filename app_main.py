314b4f2c9af12aeb40754c597a7369d77bf0af63
christopher-luu97 <christopher.luu1337@gmail.com>
2023-01-15 19:35:13 +1100

    Rename test_tts to cocqui_tts. Edit app_main to chain transcription, chatgpt and tts

--- app_main.py
+++ app_main.py
@@ -2,11 +2,20 @@
 
 from chatgpt_prompt import chatgpt_response
 from transcribe_sd_record import transcribe_recording
+from cocqui_tts import tts
 
-def main():
+def main(out_path: str):
+    """_summary_
+
+    Args:
+        out_path (str): _description_
+    """
     text_transcribed = transcribe_recording()
     text_response = chatgpt_response(text_transcribed)
-    print(text_response)
+    print("ChatGPT responded!")
+    tts(text_response, model_name = "en/ljspeech/vits", output_name =out_path)
+    print("Response saved!")
 
 if __name__ == "__main__":
-    main()
+    out_path = "data/output/temp.wav"
+    main(out_path)
