b51bc47665cc5d9b1a7390bf48a0ed4732f61f4a
christopher-luu97 <christopher.luu1337@gmail.com>
2023-01-15 14:14:50 +1100

    Edit program metadata in authoring

--- authoring/author_metadata.py
+++ authoring/author_metadata.py
@@ -8,29 +8,11 @@
 #     "description":""
 # }
 
-aswop_speech_metadata = {
-    "release_date": "01/10/2022",
-    "version_no": "1.0.0",
-    "author": "Christopher Luu",
-    "program_name": "Anti-Submarine Warfare Operations Platform - Speech AI Extension",
-    "modifications": "",
-    "description": "This software performs speech analytics on provided audio data"
-}
-
-transcription_metadata = {
-    "release_date": ": 01/12/2022",
+program_metadata = {
+    "release_date": ": dd/mm/yyyy",
     "version_no": ": 1.0.0",
     "author": ": Christopher Luu",
-    "program_name": ": Transcription module",
+    "program_name": ": M3gan",
     "modifications": ": ",
-    "description": ": This software performs speech to text transcription on audio data of .wav format"
-}
-
-metrics_metadata = {
-    "release_date": "01/12/2022",
-    "version_no": "1.0.0",
-    "author": "Christopher Luu",
-    "program_name": "Transcription error rate calculator",
-    "modifications": "",
-    "description": "This software calculates the error rate between ground truth text and transcribed text"
+    "description": ": lorem ipsum"
 }
