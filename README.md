7a5bd3e74d955d83d4d6924df8e4f9a56f772702
christopher-luu97 <christopher.luu1337@gmail.com>
2023-01-15 12:18:25 +1100

    Add initial recording and transcription code

---
+++ README.md
@@ -0,0 +1,9 @@
+Think Siri but with your choice of voice!
+This application will take your prompts and repond verbally through a variety of sound options
+
+1. Record speech from microphone
+2. Save recorded speech into a temp
+3. Transcribe saved speech in temp
+4. Pass transcribed speech to ChatGPT
+5. Store ChatGPT response
+6. Pass response to Microsoft Vall-E for speech synthesis
--- whisper_main.py
+++ whisper_main.py
@@ -2,17 +2,19 @@ from whisper_module import whisper
 import csv
 import os
 import sys
+from pathlib import Path
 import pdb
 from datetime import datetime
+from authoring.author import Author
+from authoring.author_metadata import transcription_metadata
+import time
 from datetime import datetime
 import warnings
 warnings.filterwarnings("ignore")
 
-import pathlib
 import torch
 from tqdm import tqdm
 import ffmpeg
-import datetime
 
 
 # Run the english only model as they do better for eng only apps
@@ -62,7 +64,7 @@ def transcribe_to_csv(whisper_results: dict, output_name: str):
     if ".wav" in output_name:
         output_name = output_name[:-4]
         # e.g. output_name == 'result"
-    myFile = open(output_name, 'w', newline='')
+    myFile = open(output_name, 'w')
     writer = csv.writer(myFile)
     writer.writerow(segments[0].keys())  # e.g. segments
     for dictionary in segments:  # e.g. segment in segments
@@ -110,12 +112,35 @@ def run_whisper(file_path, ffmpeg_path, basedir):
         ffmpeg_path (str): Filepath to ffmpeg exe
         basedir (str): File path to model
     """
-    model_name = "medium"#"tiny" # "medium"
-    #print(f"Execution started: {datetime.now()}")
+    model_name = "medium" # "tiny"
+    print(f"Execution started: {datetime.now()}")
     # Transcribe audio
     result = whisper_transcribe(file_path=file_path, model_name=model_name, ffmpeg_path=ffmpeg_path, basedir=basedir)
     return result
 
+
+def run_whisper_main():
+    author = Author(**transcription_metadata)
+    attrs = vars(author)
+    print("".join("%s %s\n" % item for item in attrs.items()))
+
+    model_name = "medium"
+    data_path = "data/input/"
+    data_name = input("Data name: ") + ".wav"
+    file_path = data_path + data_name
+    output_path = "./transcription_results/"
+    print(f"Execution started: {datetime.now()}")
+    start_time = time.time()
+    # Transcribe audio
+    result = whisper_transcribe(audio_file=file_path, model_name=model_name)
+
+    # Export results to csv
+    transcribe_to_csv(result['segments'],
+                      output_name=data_name, output_path=output_path)
+    transcribe_to_text(
+        result['text'], output_name=data_name, output_path=output_path)
+    end_time = time.time()
+    print(f"Total execution time: {end_time - start_time} s")
 
 class Transcriber(object):
     """
@@ -147,13 +172,15 @@ class Transcriber(object):
         self.VAD_DIR = os.path.join(AUDIO_DIR, "vad_chunks")
         print("Directory created!")
         
-    def _create_temp_audio(self):
+    def _create_temp_audio(self, ffmpeg_path: str):
         """
         Create a copy of the input audio from full_audio_path and write to a temp directory VAD_DIR
+        
+        Args:
+            ffmpeg_path (str): path to ffmpeg executable
         """
         full_audio_path = self.full_audio_path
         VAD_DIR = self.VAD_DIR
-        
         # Hardcode values to feed into ffmpeg
         # This is just a temporary copy which requires specific formatted values to ensure
         # that we get the appropriate input to the system
@@ -164,7 +191,7 @@ class Transcriber(object):
             acodec="pcm_s16le",
             map_metadata="-1",
             fflags="+bitexact",
-        ).overwrite_output().run(quiet=True)
+        ).overwrite_output().run(cmd=[ffmpeg_path, "-nostdin"], capture_stdout=True, capture_stderr=True, quiet=True)
         print(os.path.exists(os.path.join(VAD_DIR,"vad_temp.wav")))
     
     def _load_vad(self):
@@ -179,19 +206,29 @@ class Transcriber(object):
             print("Model and utils loaded!")
 
         # Store the functions within the transcriber object for easy access
-        (self.get_speech_timestamps, self.save_audio, self.read_audio, self.VADIterator, self.collect_chunks) = utils
+        (self.get_speech_timestamps, self.save_audio, self.load_audio, self.collect_chunks) = utils
 
         self.vad_model = vad_model 
     
-    def _read_audio(self):
+    def _read_audio(self, ffmpeg_path:str):
         """
         Read audio into an attribute to use
         The path here will be hardcoded as everything should be self contained
         The self-containment is from the directory and file copying from
         _vad_dir_creator() and _create_temp_audio() which if properly run
         will result in absolute file locations    
+
+        Args:
+            ffmpeg_path (str): path to ffmpeg executable
         """
-        self.wav = self.read_audio("data/input/vad_chunks/vad_temp.wav", sampling_rate=self.VAD_SR)
+        # Hardcode the location of the copied temporary wav
+        # This is read in as a np_buffer which we convert to a tensor using torch
+        audio = "data/input/vad_chunks/vad_temp.wav"
+        if not torch.is_tensor(audio):
+            if isinstance(audio, str):
+                audio = self.load_audio(audio, ffmpeg_path, sr=self.VAD_SR)
+            audio = torch.from_numpy(audio)
+        self.wav = audio
         print("Audio loaded! at %s" % ("data/input/vad_chunks/vad_temp.wav"))
     
     def _process_timestamps(self):
@@ -268,14 +305,15 @@ class Transcriber(object):
                 self.chunked_audio[i][j]["offset"] = offset
         print("Timestamps converted!")
     
-    def _whisper_on_chunks(self, ffmpeg_path:str, basedir: str, model_name:str = 'medium'):
+    def _whisper_on_chunks(self, ffmpeg_path:str, basedir: str, output_checker:bool=1, model_name:str = 'medium'):
         """
         Transcribe using whisper on the pre-processed chunked audio
         Whisper using the medium model
 
         Args:
             ffmpeg_path (str): path to ffmpeg executable e.g., os.path.join(basedir,"ffmpeg","ffmpeg.exe")
-            basedir (str): Path where executable should be located e.g., os.getcwd() 
+            basedir (str): Path where executable should be located e.g., os.getcwd()
+            output_checker (bool): Default 1 is True to show CSV, 0 for Text output
             model_name (str): Default "medium" for Whisper model size
         """
         # Load the whisper model
@@ -284,6 +322,8 @@ class Transcriber(object):
         language = 'english'
         initial_prompt = ''
 
+        segment_info = []
+        text_info = []
         # Transcribe each chunk using Whisper
         for i in tqdm(range(len(self.chunked_audio))):
             result = model.transcribe(
@@ -291,41 +331,10 @@ class Transcriber(object):
                 task=task, language=language, initial_prompt=initial_prompt
             )
             # Break if result doesn't end with severe hallucinations
-            if len(result["segments"]) == 0:
-                break
-            elif result["segments"][-1]["end"] < self.chunked_audio[i][-1]["chunk_end"] + 10.0:
-                break
-        self.result = result
-        if len(result) > 0:
-            print("Audio successfully transcribed!")
-    
-    def _run(self, ffmpeg_path:str, basedir: str):
-        """
-        This method executes all the required steps required for Whisper
-
-        Args:
-            ffmpeg_path (str): path to ffmpeg.exe 
-            basedir (str): directory to search for everyting
-        """
-        self._vad_dir_creator()
-        self._create_temp_audio()
-        self._load_vad()
-        self._read_audio()
-        self._process_timestamps()
-        self._split_audio()
-        self._merge_chunks()
-        self._convert_timestamps_seconds()
-        self._whisper_on_chunks(ffmpeg_path = ffmpeg_path, basedir = basedir)
-            
-    def whisper_csv(self, ffmpeg_path:str, basedir: str):
-        """
-        Seperate whisper results into csv only
-        """
-        self._run(ffmpeg_path = ffmpeg_path, basedir = basedir) # Execute everything needed
-        segment_info = []
-        result = self.result
-        
-        for i in tqdm(range(len(self.chunked_audio))):
+            # if len(result["segments"]) == 0:
+            #     break
+            # elif result["segments"][-1]["end"] < self.chunked_audio[i][-1]["chunk_end"] + 10.0:
+            #     break
             for r in result["segments"]:
                 # Skip audio timestamped after the chunk has ended
                 if r["start"] > self.chunked_audio[i][-1]["chunk_end"]:
@@ -335,49 +344,128 @@ class Transcriber(object):
                     continue
                 # Strip white spaces
                 r['text'] = r['text'].strip() 
-                segment_info.append(r)
-                
-        return segment_info
-    
-    def whisper_text(self, ffmpeg_path: str, basedir: str):
+                if output_checker == 1: # CSV output
+                    segment_info.append(r)
+                    self.segment_info = segment_info
+                    print(f"segment info executed with len: {len(segment_info)}")
+                elif output_checker == 0: # Text output
+                    text_info.append(r["text"])
+                    self.text_info = text_info
+                    print(f"text info executed with len: {len(text_info)}")
+
+
+        
+    def _run(self, ffmpeg_path:str, basedir: str, output_checker:bool):
         """
-        Separate the whisper results into text for output
+        This method executes all the required steps required for Whisper
+
+        Args:
+            ffmpeg_path (str): path to ffmpeg.exe 
+            basedir (str): directory to search for everyting
+            output_checker (bool): Default 1 is True to show CSV, 0 for Text output
+
         """
-        self._run(ffmpeg_path = ffmpeg_path, basedir = basedir)
-        text_info = []
-        result = self.result
+        self._vad_dir_creator()
+        print("vad_dir_creator executed!")
+        self._create_temp_audio(ffmpeg_path)
+        print("_create_temp_audio executed!")
+        self._load_vad()
+        print("_load_vad executed!")
+        self._read_audio(ffmpeg_path)
+        print("_read_audio executed!")
+        self._process_timestamps()
+        print("_process_timestamps executed!")
+        self._split_audio()
+        print("_split_audio executed!")
+        self._merge_chunks()
+        print("_merge_chunks executed!")
 
-        # Post process the segments and text based using the offsets and 
-        # identified chunk start and end to ensure we can remove overlaps
-        for i in tqdm(range(len(self.chunked_audio))):
-            for r in result["segments"]:
-                # Skip audio timestamped after the chunk has ended
-                if r["start"] > self.chunked_audio[i][-1]["chunk_end"]:
-                    continue
-                # Skip if log prob is low or no speech prob is high
-                if r["avg_logprob"] < -1.0 or r["no_speech_prob"] > 0.7: # Hardcoded thresholds
-                    continue                
-                text_info.append(r["text"].strip())
-        return text_info
+        self._convert_timestamps_seconds()
+        print("_convert_timestamps executed!")
+
+        self._whisper_on_chunks(ffmpeg_path = ffmpeg_path, basedir = basedir, output_checker=output_checker)
+        print("_whisper_on_chunks executed!")
 
 
-# To test things
-# if __name__ == '__main__':
-#     basedir = os.getcwd()
-#     ffmpeg_path = os.path.join(basedir,"ffmpeg","ffmpeg.exe")
-#     AUDIO_DIR = os.path.join(basedir, "data", "input")
-#     full_audio_path = os.path.join(basedir, "data","input","delta_orig.wav")
-#     SILERO_DIR = os.path.join(basedir, "models", "silero-vad")
-#     transcriber = Transcriber(*[SILERO_DIR, full_audio_path, AUDIO_DIR])
+    def getResult(self, output_checker:bool=1):
+        """
+        Getter for whisper result
 
-#     import pdb
-#     import json
-#     results = run_whisper(file_path = "data/input/delta_orig.wav", ffmpeg_path = ffmpeg_path, basedir = basedir)
-#     # with open("results.json", 'w') as fp:
-#     #     json.dump(results['segments'], fp)
+        Args:
+            output_checker (bool): Default 1 is True to show CSV, 0 for Text output
+
+        """
+        if output_checker == 1:
+            segment_info = self.segment_info
+            return segment_info
+        elif output_checker == 0:
+            text_info = self.text_info
+            return text_info
+        else:
+            print("No valid output selected")
     
-#     segments_info = transcriber.whisper_csv(ffmpeg_path = ffmpeg_path, basedir = basedir)
-#     # with open("segments_info.json", "w") as fp:
-#     #     json.dump(segments_info, fp)
-#     # text_info = transcriber.whisper_text(ffmpeg_path = ffmpeg_path, basedir = basedir)
-#     #pdb.set_trace()
+    def transcribe_to_csv(self, segments:list, output_name: str, output_checker:int=1):
+        """
+        Write outputs to CSV row by row from within a list of dictionaries.
+
+        Assuming the list contains dictionaries all with the same keys
+        Then write to csv with keys as a header
+        Args:
+            segments (list): List of dictionaries for the results
+            output_name (str): audio_file name for automatic file name creation.
+                                Should not be manual.
+            output_checker (int): Default 1
+        """
+        
+        if (len(segments) == None) or (len(segments) == 0):
+            print("Empty list. Please check transcription worked")
+            sys.exit()
+        if ".wav" in output_name:
+            output_name = output_name[:-4]
+            # e.g. output_name == 'result"
+        myFile = open(output_name, 'w')
+        writer = csv.writer(myFile)
+        writer.writerow(segments[0].keys())  # e.g. segments
+        for dictionary in segments:  # e.g. segment in segments
+            writer.writerow(dictionary.values())
+        myFile.close()
+        print("Transcription successfully written to file!")
+
+
+    def transcribe_to_text(self, text_info: list,output_name: str, output_checker:int=0):
+        """
+        Transcribe to text
+
+        Args:
+            text_info (list): Output string that is in a list to be concatenated
+            output_name (str): audio_file name for automatic file name creation.
+                                Should not be manual.
+            output_checker (int): Default 0 for text
+        """
+        # self.text_info is ["Hello", "World"] so we need to join them with space
+        # This will result in "Hello World" instead of "HelloWorld" when written to text
+        transcribed_text = " ".join(text_info)
+        if (len(transcribed_text)) == None or (len(transcribed_text)) == 0:
+            print("Empty string. Check that transcription has output")
+        if ".wav" in output_name:
+            output_name = output_name[:-4]
+        with open(output_name, "w") as text_file:
+            text_file.write(transcribed_text)
+
+if __name__ == "__main__":
+    full_audio_path = 'C:/Users/k66gu/Documents/transcription_dev/data/input/delta_10.wav'
+    basedir = os.getcwd()
+    AUDIO_DIR = os.path.join(basedir, "data", "input")
+    VAD_DIR = os.path.join(AUDIO_DIR,"vad_chunks")
+
+    print(os.path.exists(full_audio_path),os.path.exists(AUDIO_DIR), os.path.exists(VAD_DIR))
+
+    ffmpeg.input(full_audio_path).output(
+        VAD_DIR+"/vad_temp_2.wav",
+        ar="16000",
+        ac="1",
+        acodec="pcm_s16le",
+        map_metadata="-1",
+        fflags="+bitexact",
+    ).overwrite_output().run(quiet=True)
+    print(os.path.exists(os.path.join(VAD_DIR,"vad_temp_2.wav")))
